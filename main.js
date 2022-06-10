ed2bf37cd880ee5e9c79f1b7ab53e3b50ea9e190
sharanoth <sharanoth@guidedflame.dev>
2022-06-10 21:34:02 +0100

    boo!

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 var bgm_on = 0;
 
-function sexyfunction(){
-    if(bgm_on == 0){
-        var audio = new Audio('media/guidedflame.mp3');
-        audio.loop = true;
-        audio.play();
-        bgm_on = 1;
-    }
+function sexyfunction() {
+	var landingdiv = document.getElementById("landingdiv");
+	var maindiv = document.getElementById("maindiv");
+
+	var audio = new Audio("media/guidedflame.mp3");
+	audio.loop = true;
+
+	if (bgm_on == 0) {
+		bgm_on = 1;
+		landingdiv.style.opacity = 0;
+		setTimeout(function () {
+			audio.play();
+
+			landingdiv.setAttribute("hidden", "true");
+			maindiv.style.visibility = "visible";
+			maindiv.style.opacity = 1;
+		}, 500);
+	}
 }
