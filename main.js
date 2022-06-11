f459106828dbd0f0d8aa3872234c67faea9f63d7
sharanoth <sharanoth@guidedflame.dev>
2022-06-11 04:58:20 +0100

    added fonts added japanese translation for landing text added categories

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var bgm_on = 0;
 function sexyfunction() {
 	var landingdiv = document.getElementById("landingdiv");
 	var maindiv = document.getElementById("maindiv");
+	var maindivbg = document.getElementById("maindivbg");
+	var body = document.getElementById("body");
 
 	var audio = new Audio("media/guidedflame.mp3");
 	audio.loop = true;
@@ -14,8 +16,15 @@ function sexyfunction() {
 			audio.play();
 
 			landingdiv.setAttribute("hidden", "true");
+
 			maindiv.style.visibility = "visible";
 			maindiv.style.opacity = 1;
+			maindiv.style.width = "fit-content";
+
+			maindivbg.style.width = "100vw";
+			maindivbg.style.visibility = "visible";
+			maindivbg.style.opacity = 1;
+			maindivbg.style.filter = "blur(0px)";
 		}, 500);
 	}
 }
